256cc82ed9f36bb8b84aaf659360b18bde7a56c9
roopha <80938105+roopgha@users.noreply.github.com>
2023-03-06 10:18:38 +0900

    Add ChangeRoom

--- Assets/Scripts/UI/RoomChanger.cs
+++ Assets/Scripts/UI/RoomChanger.cs
@@ -2,26 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class RoomChanger : MonoBehaviour
 {
+    private int roomNumber;
+
+    public GameObject[] Rooms;
     [SerializeField] private Image fadeImage;
     [SerializeField] private float fadeSpeed=1;
+    [SerializeField] private float fadeChangeSpeed = 1;
+
     private Color fade;
 
     private void Awake()
     {
         fade = fadeImage.color;
+        Rooms = GameObject.FindGameObjectsWithTag("Room");
+        for(int i=0; i<Rooms.Length; i++){
+            if(i != roomNumber)
+            {
+                Rooms[i].SetActive(false);
+            }
+        }
     }
 
-    public void  FadeInFadeOut(){
-        StartCoroutine("FadeInOut");
+    
+
+    public void RoomNumber(int next){
+        StartCoroutine(FadeInOut(next));
     }
 
-    private IEnumerator FadeInOut(){
+    private IEnumerator FadeInOut(int next){
         StartCoroutine(FadeIn());
-        yield return new WaitForSeconds(1 / fadeSpeed);
+        yield return new WaitForSeconds(1 / fadeChangeSpeed);
+        ChangeRoom(next);
         StartCoroutine(FadeOut());
+    }
+
+    private void ChangeRoom(int next){
+        Rooms[roomNumber].SetActive(false);
+        roomNumber+=next;
+        if(roomNumber>Rooms.Length){
+            roomNumber=0;
+        }
+        Rooms[roomNumber].SetActive(true);
     }
 
     private IEnumerator FadeIn()
---
+++ Assets/Scripts/UI/UITextTypingEffect.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITextTypingEffect : MonoBehaviour
+{
+    Text txt;
+    string story;
+
+    void Awake(){
+        txt = GetComponent<Text> ();
+        story = txt.text;
+        txt.text = "";
+
+        StartCoroutine ("PlayText");
+    }
+
+    IEnumerator PlayText()
+    {
+        foreach(char c in story){
+            txt.text += c;
+            yield return new WaitForSeconds (0.125f);
+        }
+    }
+}
